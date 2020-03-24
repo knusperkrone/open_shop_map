857e22075a6afb94826ae7fc37ac6138f2985b47
Aron Metzig <aron.metzig@fau.de>
2020-03-24 01:09:34 +0100

    added file logging

--- rust-backend/src/logging.rs
+++ rust-backend/src/logging.rs
@@ -1,9 +1,12 @@
 use once_cell::sync::Lazy;
+use slog::{FnValue, *};
 use sloggers::{
     terminal::{Destination, TerminalLoggerBuilder},
     types::Severity,
     Build,
 };
+use std::fs::OpenOptions;
+use std::sync::Mutex;
 
 pub static APP_LOGGING: Lazy<slog::Logger> = Lazy::new(|| {
     let mut builder = TerminalLoggerBuilder::new();
@@ -11,8 +14,21 @@ pub static APP_LOGGING: Lazy<slog::Logger> = Lazy::new(|| {
         builder.level(Severity::Debug);
         builder.destination(Destination::Stdout);
     } else {
-        builder.level(Severity::Debug);
-        builder.destination(Destination::Stdout);
+        let logfile = "./log/log_app.txt";
+        let file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .append(true)
+            .open(logfile)
+            .unwrap();
+        let filter_level = "info".parse::<Level>().expect("Invalid log level filter");
+        Logger::root(
+            Mutex::new(LevelFilter::new(slog_bunyan::default(file), filter_level)).fuse(),
+            o!("location" => FnValue(move |info| {
+                format!("{}:{} {}", info.file(), info.line(), info.module())
+                })
+            ),
+        );
     }
 
     builder.build().unwrap()
